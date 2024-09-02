9b983e8c3ca86be06191a6aec756d5a627c98d51
Revati Tiwari <revatitiwari37@gmail.com>
2024-09-02 21:26:07 +0530

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,11 @@
+//find factorial of a number
+
+function factorial(n) {
+    if(n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(5))
